6851dd4fd51fdcedf8f6848f6306c15f47df6829
larenzzx <larenztabotabo@gmail.com>
2025-06-15 22:42:36 +0800

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import { Dashboard } from "./features/dashboard/Dashboard";
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-    {/* Uncomment the line below to use the Login component directly */}      
-      {/* <Login /> */}
-      {/* <Dashboard /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </>
   );
 }
